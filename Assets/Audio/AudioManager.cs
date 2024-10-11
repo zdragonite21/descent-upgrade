3cacb1cebcf5a6ba6d4cf487826efc41a053d51d
Bean-Github <kevinw11235@gmail.com>
2024-10-10 23:16:36 -0400

    Audio stuff

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sound[] sounds;
+
+    private void Awake()
     {
-        
+        foreach (Sound s in sounds)
+        {
+            AudioSource newSound = gameObject.AddComponent<AudioSource>();
+
+            newSound.clip = s.audioClip;
+
+            newSound.volume = s.volume;
+            newSound.pitch = s.pitch;
+
+            newSound.loop = s.isLooping;
+
+            if (newSound.playOnAwake)
+            {
+                newSound.Play();
+            }
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void PlaySound(string name)
     {
-        
+        Sound s = Array.Find(sounds, s => s.name == name);
+
+        if (s==null)
+        {
+            Debug.LogError("Sound " + name + " not found!");
+            return;
+        }
+
+        s.audioSource.Play();
     }
+
 }
