6880cf4d11577c3dc432a65484f36822b355993b
Bean-Github <kevinw11235@gmail.com>
2024-10-26 15:23:49 -0400

    Update WeatherManager.cs

--- Assets/Kevin Stuff/Weather/WeatherManager.cs
+++ Assets/Kevin Stuff/Weather/WeatherManager.cs
@@ -4,37 +4,37 @@ using UnityEngine;
 
 public class WeatherManager : MonoBehaviour
 {
-    public WeatherState CurrentWeather
-    {
-        get => m_CurrentState;
-        set
-        {
-            switch (value)
-            {
-                case WeatherState.Grounded:
-                    OnGrounded();
-                    m_UpdateHandler = GroundedBehavior;
-                    break;
-                case WeatherState.Midair:
-                    OnMidair();
-                    m_UpdateHandler = MidairBehavior;
-                    break;
-                default:
-                    break;
-            }
+    //public WeatherState CurrentWeather
+    //{
+    //    get => m_CurrentState;
+    //    set
+    //    {
+    //        switch (value)
+    //        {
+    //            case WeatherState.Snow:
+    //                OnGrounded();
+    //                m_UpdateHandler = GroundedBehavior;
+    //                break;
+    //            case WeatherState.Sandstorm:
+    //                OnMidair();
+    //                m_UpdateHandler = MidairBehavior;
+    //                break;
+    //            default:
+    //                break;
+    //        }
 
-            m_CurrentState = value;
-        }
-    }
-    private WeatherState m_CurrentState;
+    //        m_CurrentState = value;
+    //    }
+    //}
+    //private WeatherState m_CurrentState;
 
     private delegate void UpdateHandler();
     private UpdateHandler m_UpdateHandler;
 
     public enum WeatherState
     {
-        Grounded,
-        Midair
+        Snow,
+        Sandstorm
     }
 
 
